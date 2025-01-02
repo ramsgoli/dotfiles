ac3254ef483ea92d7c40db4d0dfaf2e150cdc4a7
Ram Goli <ram@contentsquare.com>
2025-01-02 09:11:12 -0800

    kitty improvements

--- nvim/lua/ram/lsp/setup.lua
+++ nvim/lua/ram/lsp/setup.lua
@@ -15,7 +15,7 @@ lspconfig.terraformls.setup({
   on_attach = handlers.on_attach
 })
 
-lspconfig.bufls.setup {
+lspconfig.buf_ls.setup {
   root_dir = util.root_pattern("buf.yaml", ".git"),
   on_attach = handlers.on_attach
 }
@@ -134,20 +134,5 @@ null_ls.register({
         }
       end
     end,
-  },
-})
-
--- configure tsserver
-require("typescript").setup({
-  disable_commands = false, -- prevent the plugin from creating Vim commands
-  debug = true,             -- enable debug logging for commands
-  go_to_source_definition = {
-    fallback = true,        -- fall back to standard LSP definition on failure
-  },
-  server = {                -- pass options to lspconfig's setup method
-    on_attach = function(client, bufnr)
-      client.server_capabilities.documentFormattingProvider = false
-      handlers.on_attach(client, bufnr)
-    end
   },
 })
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,3 +1,25 @@
+# +##########
+# .zshrc used for setting up interactive sessions only
+##########
+
+# set up completions
+autoload -U compinit; compinit
+
+# kitty options
+# disabled the automatic shell integration and sets it up manually, so that kitty config is applied in sub-shells
+# See https://sw.kovidgoyal.net/kitty/shell-integration/#manual-shell-integration
+if test -n "$KITTY_INSTALLATION_DIR"; then
+  export KITTY_SHELL_INTEGRATION="enabled"
+  autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+  kitty-integration
+  unfunction kitty-integration
+fi
+
+# append new entries to history file as soon as they're entered
+setopt inc_append_history
+export HISTSIZE=10000
+export SAVEHIST=10000
+
 # fzf options
 export FZF_DEFAULT_COMMAND='rg --hidden -l ""'
 export FZF_DEFAULT_OPTS='--height 40% --layout=reverse'
