60dc59c786d5f3d5e551e72e6410c5af9c8cbabf
Ram Goli <ram@contentsquare.com>
2025-03-05 10:33:02 -0800

    move floating-input config

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,7 +2,6 @@ require('ram.keymaps')
 require('ram.config')
 
 require('ram.lazy')
-require('ram.input')
 require('ram.colors')
 require('ram.commands')
 require('ram.autocommands')
--- nvim/lua/ram/plugins/floating_input.lua
+++ nvim/lua/ram/plugins/floating_input.lua
@@ -1,3 +1,10 @@
 return {
-  'liangxianzhe/floating-input.nvim'
+  'liangxianzhe/floating-input.nvim',
+  config = function()
+    local floating_intput = require('floating-input')
+    floating_intput.setup()
+    vim.ui.input = function(opts, on_confirm)
+      floating_intput.input(opts, on_confirm, { border = 'double' })
+    end
+  end
 }
